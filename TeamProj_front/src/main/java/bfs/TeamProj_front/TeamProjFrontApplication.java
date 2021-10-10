f337e296953e38ac1cd8bcf584ce4d73c553f8d6
CENTRAL\kim4t <kim4t@cmich.edu>
2021-10-10 17:47:29 -0400

    Empty files

---
+++ TeamProj_front/src/main/java/bfs/TeamProj_front/TeamProjFrontApplication.java
@@ -0,0 +1,13 @@
+package bfs.TeamProj_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjFrontApplication.class, args);
+	}
+
+}
---
+++ TeamProj_front/src/test/java/bfs/TeamProj_front/TeamProjFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package bfs.TeamProj_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
