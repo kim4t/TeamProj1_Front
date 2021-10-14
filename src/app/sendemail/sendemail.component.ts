23c65e5119e7ec845b30d40e3e7b13545fc8db0f
bruceshenqqe <bruceshenqqeq@gmail.com>
2021-10-14 09:58:27 -0400

    Update sendemail.component.ts

--- src/app/sendemail/sendemail.component.ts
+++ src/app/sendemail/sendemail.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, NgModule, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http';import { NgForm } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -15,24 +16,22 @@ export class SendemailComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(form: any)
-  {
-      /** POST: add a new e-mail to the database */
-      /** default url 8081, you could specify path in future */
-    let data  = new FormData();
+  onSubmit(form: any) {
+    /** POST: add a new e-mail to the database */
+    /** default url 8081, you could specify path in future */
+    let data = new FormData();
     data.append('e-mail', form['e-mail']);
-    this.http.post('http://localhost:8081/HR/email', data, {responseType: 'text'})
-    .subscribe((result)=>{
-      console.log(result);
-    })
+    this.http.post('http://localhost:8081/HR/email', data, { responseType: 'text' })
+      .subscribe((result) => {
+        console.log(result);
+      })
   }
-  
-  ngAfterViewInit() 
-  {
+
+  ngAfterViewInit() {
     /** this function is for color change, but there is a bug
      *  When we go back to last page, the background-color reflects.
-     * */ 
+     * */
     this.elementRef.nativeElement.ownerDocument
-        .body.style.backgroundColor = '#76b852';
+      .body.style.backgroundColor = '#76b852';
   }
 }
